61c2843c17d077636f2b1476a0907a67f31961c0
will vuong <will@willvuong.com>
2012-07-03 01:39:30 -0400

    use heatermeter calculations
    heatermeter temp calculations are way more accurate for et-732 probe
    rework and clean up display temp code

--- bbqsmoker.ino
+++ bbqsmoker.ino
@@ -66,22 +66,22 @@ void setup() {
   Probe p1 = Probe();
   p1.name = "ET-732";
   p1.pin = A1;
-  /*
   p1.A = 5.36924e-4;
   p1.B = 1.91396e-4;
   p1.C = 6.60399e-8;
-  */
-  p1.A = 0.00043933992;
-  p1.B = 0.000208342;
-  p1.C = 1.2004001E-8;
+
   p1.resistor = 10000;
   probes[1] = p1;
   
   // loop through probes and print out a debug message
+  lcd.clear();
+  lcd.autoscroll();
+  lcd.setCursor(16,0);
   for (int i = 0; i < numProbes; i++) {
      Probe *p = &probes[i];
      if (&p != 0) {
        Serial.print((*p).name); Serial.print(" initialized at pin ") + Serial.println((*p).pin);
+       lcd.write("p"); lcd.print(i); lcd.print("="); lcd.print((*p).pin); lcd.write(" ");
      }
   }
   
@@ -97,17 +97,12 @@ void loop() {
     Probe *ptr = &probes[i];
     
     // read the analog in value:
-    sensorValue = readAndOverSample((*ptr).pin);
-  
-    // map it to the range of the analog out:
-    outputValue = thermister_temp(sensorValue, (*ptr).A, (*ptr).B, (*ptr).C, (*ptr).resistor);
-    
-    // record it
+    outputValue = readAndOverSample((*ptr).pin, (*ptr).A, (*ptr).B, (*ptr).C, (*ptr).resistor);
+
+    // record it into temperature reading buffer
     (*ptr).currentTemp = outputValue;
     // push it onto the temp buffer
     // everything on the array gets shifted over one
-    // Serial.print("last temp: "); Serial.println((*ptr).buffer[0]);
-    // Serial.print("direct array access: "); Serial.println((*ptr).buffer[0]);
     for (int i = bufferLength -1; i >= 1; i--) {
       (*ptr).buffer[i] = (*ptr).buffer[i-1];
     } 
@@ -117,84 +112,106 @@ void loop() {
       Serial.print((*ptr).buffer[i]); Serial.print(" ");
     }
     Serial.println();
-    */
-    // print the results to the serial monitor:
-    Serial.print("sensor = " );                       
-    Serial.print(sensorValue);      
-    Serial.print("\t output = ");      
-    Serial.println(outputValue);
-    
+    */    
   }
   
   // print all probe summary
+  lcd.clear();
+  lcd.noAutoscroll();
   
-  
-  // fill up sixty seconds with display
-  int per = 20000/numProbes/5000;
-  for (int p = 0; p < per; p++) {
   for (int i = 0; i < numProbes; i++) {
     Probe *ptr = &probes[i];
     
-    // calculate last five minute average
-    int five, fifteen, thirty;
-    int accum = 0;
-    for (int i = 0; i < 5; i++) {
-      if ((*ptr).buffer[i] == 0) {
-        accum = 0;
-        break;
-      }
-      accum += (*ptr).buffer[i];
+    // this only supports max 4 probes
+    int x; int y;
+    if (i % 2 == 0) {
+      x = 0;
     }
-    five = accum/5;
-    
-    // calculate last 15 minute average
-    accum = 0;
-    for (int i = 0; i < 15; i++) {
-      if ((*ptr).buffer[i] == 0) {
-        accum = 0;
-        break;
-      }
-      accum += (*ptr).buffer[i];
+    else if (i % 2 == 1) {
+      x = 8; 
     }
-    fifteen = accum/15;
-    
-    // calculate last 30 minute average
-    accum = 0;
-    for (int i = 0; i < 30; i++) {
-      if ((*ptr).buffer[i] == 0) {
-        accum = 0;
-        break;
-      }
-      accum += (*ptr).buffer[i];
+    if (i / 2 == 0) {
+      y = 0;
     }
-    thirty = accum/30;
+    else if (i / 2 == 1) {
+      y = 1; 
+    }
     
-    lcd.clear();                  // start with a blank screen
-    lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
-    
-    // first row, write probe id, name, and current temp
-    // write degree symbol http://arduino.cc/forum/index.php?topic=94914.0
-    lcd.print("p"); lcd.print(i); lcd.print(" "); lcd.print((*ptr).name); lcd.print(" "); lcd.print((*ptr).currentTemp); lcd.write((uint8_t)0);
-    
-    // second row, write last 5 minutes
-    lcd.setCursor(0,1);           // set cursor to column 0, row 1
-    lcd.print(five); lcd.print(" "); lcd.print(fifteen); lcd.print(" "); lcd.print(thirty);
-    lcd.noAutoscroll();
-    
-    delay(5000);
+    lcd.setCursor(x, y);
+    lcd.print("p"); lcd.print(i); lcd.print("="); lcd.print((*ptr).currentTemp); lcd.write((uint8_t)0); lcd.print(" ");
   }
+  delay(10000);
+  
+  // fill up sixty seconds with display
+  int per = 50000/numProbes/5000;
+  
+  for (int p = 0; p < per; p++) {
+    for (int i = 0; i < numProbes; i++) {
+      Probe *ptr = &probes[i];
+      
+      // calculate last five minute average
+      int five, fifteen, thirty;
+      int accum = 0;
+      for (int i = 0; i < 5; i++) {
+        if ((*ptr).buffer[i] == 0) {
+          accum = 0;
+          break;
+        }
+        accum += (*ptr).buffer[i];
+      }
+      five = accum/5;
+      
+      // calculate last 15 minute average
+      accum = 0;
+      for (int i = 0; i < 15; i++) {
+        if ((*ptr).buffer[i] == 0) {
+          accum = 0;
+          break;
+        }
+        accum += (*ptr).buffer[i];
+      }
+      fifteen = accum/15;
+      
+      // calculate last 30 minute average
+      accum = 0;
+      for (int i = 0; i < 30; i++) {
+        if ((*ptr).buffer[i] == 0) {
+          accum = 0;
+          break;
+        }
+        accum += (*ptr).buffer[i];
+      }
+      thirty = accum/30;
+      
+      lcd.clear();                  // start with a blank screen
+      lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
+      
+      // first row, write probe id, name, and current temp
+      // write degree symbol http://arduino.cc/forum/index.php?topic=94914.0
+      lcd.print("p"); lcd.print(i); lcd.print(" "); lcd.print((*ptr).name); lcd.print(" "); lcd.print((*ptr).currentTemp); lcd.write((uint8_t)0);
+      
+      // second row, write last 5 minutes
+      lcd.setCursor(0,1);           // set cursor to column 0, row 1
+      lcd.print(five); lcd.write((uint8_t)0); lcd.print(" "); lcd.print(fifteen); lcd.write((uint8_t)0); lcd.print(" "); lcd.print(thirty); lcd.write((uint8_t)0);
+      lcd.noAutoscroll();
+      
+      delay(5000);
+    }
   }
 }
 
+// function was based on http://hruska.us/tempmon/BBQ_Controller.pde for maverick bbq probes, then rewritten
+// http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1246091012
+// http://www.kpsec.freeuk.com/vdivider.htm
 // http://en.wikipedia.org/wiki/Oversampling
-// https://github.com/CapnBry/HeaterMeter/blob/c05dc0c39672f12aaf25314c2dfe46a51fb3535d/arduino/heatermeter/grillpid.cpp
-int readAndOverSample(int apin) {
+int readAndOverSample(int apin, double A, double B, double C, int resistor) {
   int sensorvalue = 0;
   unsigned int accumulated = 0;
   int numsamples = 64; // to get 3 more bits of precision from 10 to 13, 2^(2*3) = 64 samples
   int n = 0;
   
   sensorvalue = analogRead(apin);
+  Serial.print("pin: "); Serial.println(apin);
   Serial.print("initial read: "); Serial.println(sensorvalue);
   delay(2);
   
@@ -214,40 +231,20 @@ int readAndOverSample(int apin) {
     delay(2);
   }
   
-  unsigned int oversampled = ((float)(accumulated >> 3)/((1 << 13) - 1)) * 1024;
-  Serial.print("accumulated="); Serial.print(accumulated); Serial.print(", "); Serial.print(n); Serial.print("x, oversampled="); Serial.println(oversampled);
-  return oversampled;
-}
-
-// function copied from http://hruska.us/tempmon/BBQ_Controller.pde for maverick bbq probes
-// http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1246091012
-// http://www.kpsec.freeuk.com/vdivider.htm
-
-
-/*
-925.00 voltage=4.52 R=207,424.20
-sensor = 925	 output = 75
-
-1023.00 voltage=5.00 R=22,710,622.00
-sensor = 1023	 output = -89
-*/
-int thermister_temp(int aval, double A, double B, double C, int resistor) {
-  double logR, R, T;
-
-  // This is the value of the other half of the voltage divider
-  //	Rknown = 22200;
-  
-  float voltage = aval/1024.0*5.0;
-  
-  // Do the log once so as not to do it 4 times in the equation
-  //	R = log(((1024/(double)aval)-1)*(double)22200);
-  R = (1 / ((1024 / (double) aval) - 1)) * (double) resistor; // this calculation is for vcc -> 22k resistor -> analog in -> thermister -> ground
-  logR = log(R);
-  Serial.print(aval); Serial.print(" voltage="); Serial.print(voltage); Serial.print(" R="); Serial.print(R); Serial.print("\n");
-  
-  // Compute degrees C from Kelvin; T = 1 / (A + B log(R) + C log(R) ^ 3)
-  T = (1 / (A + B * logR + C * logR * logR * logR)) - 273.25;
-  
-  // return degrees F
-  return ((int) ((T * 9.0) / 5.0 + 32.0));
+  Serial.print("accumulated="); Serial.print(accumulated); Serial.print(", "); Serial.print(n); Serial.print("x"); Serial.println();
+ 
+  // https://github.com/CapnBry/HeaterMeter/blob/c05dc0c39672f12aaf25314c2dfe46a51fb3535d/arduino/heatermeter/grillpid.cpp 
+  unsigned int ADCmax = (1 << 13) - 1;
+  unsigned int ADCval = accumulated >> 3;
+  Serial.print("ADCmax="); Serial.print(ADCmax); Serial.print(", "); Serial.print("ADCval="); Serial.print(ADCval); Serial.println();
+  float R, T;
+  R = log( resistor / ((ADCmax/(float)ADCval) - 1.0f));
+  // R = log(C * ADCmax / (float)5.0 - C);
+  Serial.print("R="); Serial.println(R);
+  T = 1.0f / ((C * R * R + B) * R + A);
+  Serial.print("T="); Serial.println(T);
+  float temp = ((T - 273.15f) * (9.0f / 5.0f)) + 32.0f;
+  Serial.print("temp="); Serial.println(temp);
+  Serial.println();
+  return temp;
 }
